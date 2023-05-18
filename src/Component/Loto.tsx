f3c0549fa4669d435d7bb199a04e3cb5c2eea7bb
elaman sargaldaev <melisbekovelaman311@gmail.com>
2023-05-18 17:01:48 +0600

    add component Loto.tsx

---
+++ src/Component/Loto.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface LotteryBallProps {
+    number: number;
+}
+
+const LotteryBall: React.FC<LotteryBallProps> = ({ number }) => {
+    return <div className="lottery-ball">{number}</div>;
+};
+
+export default LotteryBall;
